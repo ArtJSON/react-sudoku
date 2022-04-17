b33a4bbeaf769b6b8b00872e2c2031984bc23513
ArtJSON <artur.pietrzak1@outlook.com>
2022-04-18 00:09:59 +0200

    Create react app backbone

---
+++ src/Sudoku/Board.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import Field from "./Field";
+
+class Board extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+export default Board;
---
+++ src/Sudoku/SudokuGame.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import Board from "./Board";
+
+class SudokuGame extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+export default SudokuGame;
